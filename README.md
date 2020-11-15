683865321869d1b7075c165a45dd9a2478bf1838
lonePatient <1436496575@qq.com>
2020-11-15 21:35:12 +0800

    update code

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# NeZha_Chinese_PyTorch
+## NeZha_Chinese_PyTorch
+
+pytorch版NEZHA，适配transformers
+
+论文下载地址: [NEZHA: Neural Contextualized Representation for Chinese Language Understanding](https://arxiv.org/abs/1909.00204)
+
+### 运行脚本依赖模块
+
+如果需要运行该example脚本，需要安装以下模块：
+
+1. trainsformers>=2.10.0
+2. [TorchBlocks](https://github.com/lonePatient/TorchBlocks)
+
+### 模型权重下载
+
+官方提供的Tensorflow版本权重下载地址：[huawei-noah](https://github.com/huawei-noah/Pretrained-Language-Model/tree/master/NEZHA-TensorFlow)
+
+已经转化为PyTorch版本权重下载地址如下：
+
+* nezha-cn-base [百度网盘链接](https://pan.baidu.com/s/1sPC-FZJ20RtTEw9UX_4sDw) 提取码: hckq 
+
+* nezha-large-zh [百度网盘链接](https://pan.baidu.com/s/1ASg6xJeaO6dfxdeq0ozZ5w) 提取码: qks2
+
+* nezha-base-www [百度网盘链接](https://pan.baidu.com/s/1itZ_wdU6JdpXx2saK_zQhw) 提取码: ysg3
+
+* nezha-large-www [百度网盘链接](https://pan.baidu.com/s/1_QdimUFM9dD3q4JtAlAU3g) 提取码: 8dig
+
+### 运行
+
+执行命令:
+```shell
+sh scripts/run_task_text_classification_chnsenti.sh
+```
+### 结果
+
+| NEZHA(base-www) | chnsenti  |
+| --------------- | --------- |
+| tensorflow      | 94.75     |
+| pytorch         | **94.92** |
+
+
---
+++ convert_nezha_original_tf_checkpoint_to_pytorch.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+# Copyright 2018 The HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Convert ALBERT checkpoint."""
+
+import argparse
+import logging
+import torch
+from model.modeling_nezha import NeZhaConfig, NeZhaForPreTraining, load_tf_weights_in_nezha
+
+logging.basicConfig(level=logging.INFO)
+
+def convert_tf_checkpoint_to_pytorch(tf_checkpoint_path, nezha_config_file, pytorch_dump_path):
+    # Initialise PyTorch model
+    config = NeZhaConfig.from_json_file(nezha_config_file)
+    print("Building PyTorch model from configuration: {}".format(str(config)))
+    model = NeZhaForPreTraining(config)
+    # Load weights from tf checkpoint
+    load_tf_weights_in_nezha(model, config, tf_checkpoint_path)
+    # Save pytorch-model
+    print("Save PyTorch model to {}".format(pytorch_dump_path))
+    torch.save(model.state_dict(), pytorch_dump_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Required parameters
+    parser.add_argument(
+        "--tf_checkpoint_path", default=None, type=str, required=True, help="Path to the TensorFlow checkpoint path."
+    )
+    parser.add_argument(
+        "--nezha_config_file",
+        default=None,
+        type=str,
+        required=True,
+        help="The config json file corresponding to the pre-trained ALBERT model. \n"
+             "This specifies the model architecture.",
+    )
+    parser.add_argument(
+        "--pytorch_dump_path", default=None, type=str, required=True, help="Path to the output PyTorch model."
+    )
+    args = parser.parse_args()
+    convert_tf_checkpoint_to_pytorch(args.tf_checkpoint_path, args.nezha_config_file, args.pytorch_dump_path)
+
+
+'''
+python convert_nezha_original_tf_checkpoint_to_pytorch.py \
+    --tf_checkpoint_path=./pretrained_models/nezha-large-www \
+    --nezha_config_file=./pretrained_models/nezha-large-www/config.json \
+    --pytorch_dump_path=./pretrained_models/nezha-large-www/pytorch_model.bin
+
+'''
